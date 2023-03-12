1c5c21866dbdcde81b0633b78e99390cd9de5a4f
kondy <kondy@Laptop-Yerik>
2023-03-12 12:51:21 -0600

    1er Commit con Controlador Vista y Main

---
+++ Proyecto1Datos1/src/proyecto1datos1/FXMLController.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package proyecto1datos1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author kondy
+ */
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Button btDummy;
+    @FXML
+    private Button btAdvanced;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void newDummy(ActionEvent event) {
+    }
+
+    @FXML
+    private void newAdvanced(ActionEvent event) {
+    }
+    
+}
