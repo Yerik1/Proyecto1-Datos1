5f64206325ebe8ee4ccd5ed7961618afd4c04ecd
Yerik <>
2023-04-04 15:03:19 -0600

    Prueba documentacion clase Proyecto1Datos1

--- Proyecto1Datos1/src/proyecto1datos1/Proyecto1Datos1.java
+++ Proyecto1Datos1/src/proyecto1datos1/Proyecto1Datos1.java
@@ -12,22 +12,27 @@ import javafx.stage.Stage;
 import static javafx.application.Application.launch;
 
 /**
- *
- * @author kondy
+ * Clase Main que inicializa la aplicacion abriendo el fxml principal
+ * @author Yerik
  */
 public class Proyecto1Datos1 extends Application {
     
     @Override
+    /**
+     * Metodo start que ejecuta el abrir el FXML del menu
+     */
     public void start(Stage stage) throws Exception {
+        //Declara el FXML
         Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
-        
+        //Declara la escena a abrir
         Scene scene = new Scene(root);
-        
+        //Se ejecuta el abrir la escena
         stage.setScene(scene);
         stage.show();
     }
 
     /**
+     * Metodo main para iniciar el programa
      * @param args the command line arguments
      */
     public static void main(String[] args) {
